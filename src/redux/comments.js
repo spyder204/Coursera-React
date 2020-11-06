116f1d876292c83c3516cc04bbcbb466741ed804
spyder204 <vibhortiwari11@gmail.com>
2020-11-06 20:54:25 +0530

    Redux actions updated

--- src/redux/comments.js
+++ src/redux/comments.js
@@ -1,22 +1,20 @@
 import * as ActionTypes from './ActionTypes';
-const { COMMENTS } = require("../shared/comments");
 
-export const commentsReducer = (state = COMMENTS, action) => {
-    switch(action.type){
-        
-       case ActionTypes.ADD_COMMENT:
-            var comment = action.payload;
-            comment.id = state.length;
-            //comment id =  number of comments in the comments.js file
-            comment.date = new Date().toISOString();
-            
-            //can't modify the current state
-            //concat doesn't change the existing array
-            //return a new array
-            return state.concat(comment);
+export const Comments = (state = { errMess: null, comments:[]}, action) => {
+  switch (action.type) {
+    case ActionTypes.ADD_COMMENTS:
+      return {...state, errMess: null, comments: action.payload};
 
-        default:
-            return state;
-    }
+    case ActionTypes.COMMENTS_FAILED:
+      return {...state, errMess: action.payload};
 
-}
+    case ActionTypes.ADD_COMMENT:
+        var comment = action.payload;
+        comment.id = state.comments.length;
+        comment.date = new Date().toISOString();
+        return { ...state, comments: state.comments.concat(comment)};
+
+    default:
+      return state;
+  }
+};
